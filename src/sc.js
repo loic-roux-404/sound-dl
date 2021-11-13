c418c3fda442d276a164b9c14a2b4b9aca9a66fa
loic-roux <69loloro10@gmail.com>
2021-11-13 20:14:26 +0100

    finished song dl

--- src/sc.js
+++ src/sc.js
@@ -5,49 +5,52 @@ const path = require('path')
 const client = new SoundCloud.Client()
 const TRACKS = 'tracks' // default
 const LIKES = 'likes'
-const simplifiedSong = ({ url, title }) => ({ url, title })
+const simplifiedSong = ({ permalink_url, title }) => ({ uri: permalink_url, title })
 
 const getSongsInUser = async (username, field = TRACKS) => {
     return (await client.getUser(username))[field]
         .map(simplifiedSong)
 }
 
-const getSongsInPlaylist = async (url) => {
-    return (await client.getPlaylist(url))[TRACKS]
+const getSongsInPlaylist = async (uri) => {
+    return (await client.getPlaylist(uri))[TRACKS]
         .map(simplifiedSong)
 }
 
-const downloadSongs = (folder, songsUrl, count = null) => {
-    if (count) songsUrl = songsUrl.slice(0, count)
+const downloadSongs = (folder, songsUri, count = null) => {
+    if (count) songsUri = songsUri.slice(0, count)
+    console.log(songsUri)
+    songsUri.every(({ title, uri }) => {
+        console.log(uri)
+        return client.getSongInfo(uri, {})
+            .then(async song => {
+                const songDir = `${folder}/${song.author.name || song.author.username}`
+                    .replace(' ', '-')
 
-    songsUrl.every(({ title, url }) => client.getSongInfo(url)
-        .then(async song => {
-            const songDir = `${folder}/${song.author.name || song.author.username}`
-                .replace(' ', '-')
+                if (!fs.existsSync(`${songDir}/${title}.mp3`)) {
+                    if (!fs.existsSync(songDir)) {
+                        fs.mkdirSync(songDir, { recursive: true })
+                    }
 
-            if (!fs.existsSync(`${songDir}/${title}.mp3`)) {
-                if (!fs.existsSync(songDir)) {
-                    fs.mkdirSync(songDir, { recursive: true })
+                    const stream = await song.downloadProgressive();
+                    const writer = stream.pipe(
+                        fs.createWriteStream(`${songDir}/${title}.mp3`));
+                        writer.on("finish", () => {
+                            console.log("Finished writing song!")
+                            process.exit(1);
+                    });
                 }
-
-                const stream = await song.downloadProgressive();
-                const writer = stream.pipe(
-                    fs.createWriteStream(`${songDir}/${title}.mp3`));
-                    writer.on("finish", () => {
-                        console.log("Finished writing song!")
-                        process.exit(1);
-                });
-            }
-        })
-        .catch(console.error)
-    );
+            })
+            .catch(console.error)
+    });
 }
 
 const downloadUserSongs = async ({ folder, username, field, count }) =>
     downloadSongs(folder, await getSongsInUser(username, field), count)
 
-const downloadPlaylistSongs = async ({ folder, url, count }) =>
-    downloadSongs(folder, await getSongsInPlaylist(url,), count)
+const downloadPlaylistSongs = async ({ folder, uri, count }) => {
+    downloadSongs(folder, await getSongsInPlaylist(uri), count)
+}
 
 const tracks = cnf => downloadUserSongs(cnf)
 
@@ -103,13 +106,13 @@ function main(args) {
     console.info(`Downloading in ${folder} :
 ${likesEnabled ? 'likes enabled': ''}\
 ${tracksEnabled ? 'tracks of': ''}\
-${playlistsEnabled ? 'playlist': ''}\
- ${subject} with limit of ${count} tracks
-    `)
+${playlistsEnabled ? 'playlist': ''} \
+${subject} with limit of ${count} tracks
+`)
 
     if (tracksEnabled) tracks({ folder, username: subject, count })
     if (likesEnabled) likes({ folder, username: subject, count })
-    if (playlistsEnabled) playlists({ folder, url: subject, count })
+    if (playlistsEnabled) playlists({ folder, uri: subject, count })
 
     return 0
 }
